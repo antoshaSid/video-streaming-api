1bfd190ddf09675baab5068d00af1c899eec1135
Anton Sidliar <antonsidlyar@gmail.com>
2024-06-08 12:29:57 +0300

    Implement migration-service for database schema migration

---
+++ migration-service/src/main/java/com/asidliar/migration/service/MigrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.asidliar.migration.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigrationServiceApplication.class, args);
+    }
+
+}
