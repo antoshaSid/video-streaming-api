b4bc0686912cf142dc085a8d244f2c54f295cf38
Anton Sidliar <antonsidlyar@gmail.com>
2024-06-10 00:43:11 +0300

    Fix LoadVideoDto

--- video-service/src/main/java/com/asidliar/video/service/dto/LoadVideoDto.java
+++ video-service/src/main/java/com/asidliar/video/service/dto/LoadVideoDto.java
@@ -1,11 +1,13 @@
 package com.asidliar.video.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class LoadVideoDto {
 
